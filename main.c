ff190e5dcdcfff6c25a4b14306124b2842582289
anthony <b09902033@csie.ntu.edu.tw>
2021-05-28 14:06:01 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "api.h"
 #include <stdio.h>
+#define TOKEN_MAX_LEN 1000
+#define MAX_ANSWER_LEN 10050
+
+#include "anthony.h"
 
 // The testdata only contains the first 100 mails (mail1 ~ mail100)
 // and 2000 queries for you to debug.
@@ -8,12 +12,19 @@ int n_mails, n_queries;
 mail *mails;
 query *queries;
 
+int answer[MAX_ANSWER_LEN], answerLen;
+
 int main(void) {
 	api.init(&n_mails, &n_queries, &mails, &queries);
 
+	initTokensFromMails();
+
 	for(int i = 0; i < n_queries; i++)
-	    if(queries[i].type == expression_match)
+	    if(queries[i].type == expression_match) {
+			answerLen = 0;
+            expressionMatch(queries[i].data.expression_match_data.expression);
 	        api.answer(queries[i].id, NULL, 0);
+		}
 
   return 0;
 }
